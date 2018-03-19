831ac6250244855c5698a3ee66336315c2c07de1
ytyaru0 <ytyaru0@outlook.jp>
2018-03-19 21:07:02 +0900

    memo追加

--- src/script/sh/_userlib/py_env/called_ranger.sh
+++ src/script/sh/_userlib/py_env/called_ranger.sh
@@ -1,3 +1,7 @@
 . ~/root/script/sh/_userlib/py_venv.sh ranger
 #. /tmp/work/RaspberryPi.Home.Root.20180318143826/src/script/sh/_userlib/py_venv.sh ranger
-ranger
+
+# :r でpythonファイル実行させたとき、一瞬で表示が消えてしまう問題を解決したかったが、できなかった。
+# http://malkalech.com/ranger_filer#less
+#ranger
+LESS="$LESS -+F -+X" ranger
